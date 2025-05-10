d07ea71380566d7bc8831041217c7403f35ba0c4
Joao Felipe Galvao <joaofelipe.galvao021@gmail.com>
2025-05-10 17:50:22 -0300

    feat: adiciona busca de filmes via API da OMDb com tratamento de exceções

--- src/main/java/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/main/java/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,5 +1,7 @@
 package br.com.alura.screenmatch.modelos;
 
+import br.com.alura.screenmatch.excecao.ErroDeConversaoDeAnoException;
+
 public class Titulo implements Comparable<Titulo> {
 	private String nome;
 	private int anoDeLancamento;
@@ -11,6 +13,15 @@ public class Titulo implements Comparable<Titulo> {
 	public Titulo(String nome, int anoDeLancamento) {
 		this.nome = nome;
 		this.anoDeLancamento = anoDeLancamento;
+	}
+
+	public Titulo(TituloOmdb meuTituloOmdb) {
+		this.nome = meuTituloOmdb.title();
+		if (meuTituloOmdb.year().length() > 4) {
+			throw new ErroDeConversaoDeAnoException("Não consegui converter o ano por que tem mais de 4 caracteres");
+		}
+		this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
+		this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0, 2));
 	}
 
 	public String getNome() {
@@ -66,5 +77,12 @@ public class Titulo implements Comparable<Titulo> {
 	@Override
 	public int compareTo(Titulo outroTitulo) {
 		return this.getNome().compareTo(outroTitulo.getNome());
+	}
+
+	@Override
+	public String toString() {
+		return "nome='" + nome + '\'' + ", anoDeLancamento=" + anoDeLancamento + ", incluidoNoPlano=" + incluidoNoPlano
+				+ ", somaDasAvaliacoes=" + somaDasAvaliacoes + ", totalDeAvaliacoes=" + totalDeAvaliacoes
+				+ ", duracaoEmMinutos=" + duracaoEmMinutos + '}';
 	}
 }
